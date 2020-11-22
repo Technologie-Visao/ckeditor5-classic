7e2b40cedf48ec64cbd4c9599fd7883eaf5f0948
alexdaube <alexdaube@gmail.com>
2020-11-22 15:06:18 -0500

    Making the build more lightweight

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,11 +4,6 @@
  */
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
@@ -17,20 +12,12 @@ import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Video from "@visao/ckeditor5-video/src/video";
 import VideoUpload from "@visao/ckeditor5-video/src/videoupload";
@@ -43,14 +30,8 @@ export default class ClassicVisaoEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicVisaoEditor.builtinPlugins = [
 	Autoformat,
-	BlockQuote,
 	Bold,
 	Essentials,
-	FontColor,
-	FontFamily,
-	FontSize,
-	Heading,
-	Highlight,
 	HorizontalLine,
 	Image,
 	ImageCaption,
@@ -59,18 +40,11 @@ ClassicVisaoEditor.builtinPlugins = [
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	Indent,
-	IndentBlock,
 	Italic,
 	Link,
 	List,
 	ListStyle,
 	MediaEmbed,
-	PageBreak,
-	Paragraph,
-	Table,
-	TableToolbar,
-	TextTransformation,
 	Video,
 	VideoResize,
 	VideoUpload,
@@ -82,25 +56,16 @@ ClassicVisaoEditor.builtinPlugins = [
 ClassicVisaoEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
-			'|',
+			'layout',
 			'bold',
 			'italic',
+			'horizontalLine',
 			'link',
+			'mediaEmbed',
 			'bulletedList',
 			'numberedList',
-			'|',
-			'layout',
-			'indent',
-			'outdent',
-			'|',
-			'videoUpload',
 			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
+			'videoUpload',
 		]
 	},
 	image: {
@@ -111,12 +76,8 @@ ClassicVisaoEditor.defaultConfig = {
 			'imageTextAlternative'
 		]
 	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
+	mediaEmbed: {
+		removeProviders: [ 'instagram', 'twitter', 'googleMaps', 'flickr', 'facebook', 'spotify', 'dailymotion' ]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
