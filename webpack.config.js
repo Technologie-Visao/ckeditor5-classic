6118743b78e4da93a2da9e61c53f8492afd0a284
alexdaube <alexdaube@gmail.com>
2021-06-01 20:07:44 -0400

    Upgrading custom deps to 27.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
-/**
- * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
 'use strict';
 
 /* eslint-env node */
@@ -11,7 +6,7 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const TerserWebpackPlugin = require( 'terser-webpack-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
 	devtool: 'source-map',
@@ -20,7 +15,6 @@ module.exports = {
 	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
 
 	output: {
-		// The name under which the editor will be exported.
 		library: 'ClassicEditor',
 
 		path: path.resolve( __dirname, 'build' ),
@@ -31,11 +25,10 @@ module.exports = {
 
 	optimization: {
 		minimizer: [
-			new TerserWebpackPlugin( {
+			new TerserPlugin( {
 				sourceMap: true,
 				terserOptions: {
 					output: {
-						// Preserve CKEditor 5 license comments.
 						comments: /^!/
 					}
 				},
@@ -46,8 +39,6 @@ module.exports = {
 
 	plugins: [
 		new CKEditorWebpackPlugin( {
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en',
 			additionalLanguages: ['fr']
 		} ),
@@ -76,19 +67,14 @@ module.exports = {
 						}
 					},
 					{
-						loader: 'css-loader'
-					},
-					{
 						loader: 'postcss-loader',
-						options: {
-							postcssOptions: styles.getPostCssConfig( {
-								themeImporter: {
-									themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-								},
-								minify: true
-							} )
-						}
-					},
+						options: styles.getPostCssConfig( {
+							themeImporter: {
+								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+							},
+							minify: true
+						} )
+					}
 				]
 			}
 		]
